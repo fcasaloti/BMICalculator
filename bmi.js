bc8b16a56ac748e475f32fc7d76a5f457bdd171e
fcasaloti <fcasaloti@hotmail.com>
2020-05-28 19:11:46 -0700

    Finish all the changes.

--- bmi.js
+++ bmi.js
@@ -7,22 +7,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'pug');
 
-app.use(express.static(__dirname + '/views'));
+app.use(express.static('static'));
 
 app.get('/', (req, res) => {
     res.render('bmi');
     });
-
-var data;
 
 app.post("/",(req,res) => {
     var weight = Number(req.body.weight);
     var height = Number(req.body.height);
     data = weight / Math.pow((height)/100,2);
 
-    //res.send('Your BMI is ' + bmi.toFixed(2)); //send data`
-
-    data = data.toFixed(2);
+    var data = data.toFixed(2);
 
     res.render('bmi', { data });
 
